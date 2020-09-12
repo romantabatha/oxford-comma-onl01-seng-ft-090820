1a5251d95683106cf735aaa339fbf57f53e061c5
Tabatha Roman <romantabatha17@gmail.com>
2020-09-12 02:43:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,28 @@
-def oxford_comma(array)
 
+# def oxford_comma(array)
+#   array.join
+#   array.join(" and ")
+# end
+
+# def oxford_comma(array)
+#   nil if array.nil?
+#   array[0] if array.length == 1
+#   array[0..-2].join(', ') + " and " + array[-1] if array.length > 1
+# end
+
+# def oxford_comma(array)
+#   array.to_s if array.nil? or array.length <= 1
+#   array[0..-2].join(", ") + " and " + array[-1]
+# end
+
+def oxford_comma(array)
+    if array.length == 1
+     array.join
+     elsif array.length == 2 
+     array.join(" and ")
+     elsif array.length > 2
+     array[-1].insert(0,"and ")
+     array.join(", ")
+  end
 end
+
