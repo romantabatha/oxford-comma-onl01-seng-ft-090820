0eebc1d8d95c057fa3cb1f8e4529c8ddbf723806
Tabatha Roman <romantabatha17@gmail.com>
2020-09-12 02:17:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+  array.join
+  array.join(" and ")
+end
 
+# def oxford_comma(array)
+#   nil if array.nil?
+#   array[0] if array.length == 1
+#   array[0..-2].join(', ') + " and " + array[-1] if array.length > 1
+# end
+
+# def oxford_comma(array)
+#   array.to_s if array.nil? or array.length <= 1
+#   array[0..-2].join(", ") + " and " + array[-1]
+# end
+
+def oxford_comma(array)
+  array.join(",") + array.insert(2, "and")
 end
