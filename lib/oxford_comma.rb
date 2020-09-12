62fa0cab5c5b3fd2619b4110a5b8dd19fe962cc9
Tabatha Roman <romantabatha17@gmail.com>
2020-09-12 02:33:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -19,7 +19,7 @@ def oxford_comma(array)
   if array.length == 2 
      array.join(" and ")
      elsif array.length > 2
-     array[-1].insert(0,"and")
+     array[-1].insert(0,"and ")
      binding.pry
   end
     
